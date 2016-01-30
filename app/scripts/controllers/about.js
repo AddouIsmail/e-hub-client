444ce609414d0aec5861b0c5e3567c609673f480
ayoubolk <ayoubolk@gmail.com>
2016-01-30 19:05:39 +0000

    init repository

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eHubClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the eHubClientApp
+ */
+angular.module('eHubClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eHubClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eHubClientApp
+ */
+angular.module('eHubClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
